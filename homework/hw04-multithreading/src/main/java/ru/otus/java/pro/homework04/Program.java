4c86896ae96217d8a32cbab98253a4fb660ac480
AndrewBox <AndrewBoxMail@yandex.ru>
2024-05-04 21:42:28 +0300

    Hw-04 for review

--- homework/hw04-multithreading/src/main/java/ru/otus/java/pro/homework04/Program.java
+++ homework/hw04-multithreading/src/main/java/ru/otus/java/pro/homework04/Program.java
@@ -1,6 +1,16 @@
 package ru.otus.java.pro.homework04;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        var threadPool = new CustomThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            var counter = i;
+            threadPool.execute(() -> System.out.printf("Task %s -> thread %s\n", counter, Thread.currentThread().getName()));
+        }
+
+        Thread.sleep(100);
+        threadPool.shutDown();
+
+        threadPool.execute(() -> System.out.printf("Task %s -> thread %s\n", 10, Thread.currentThread().getName()));
     }
 }
