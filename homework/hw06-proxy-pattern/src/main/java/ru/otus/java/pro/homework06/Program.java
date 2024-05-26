90c96731c7d8ad92f639b55b19e208187963700f
AndrewBox <AndrewBoxMail@yandex.ru>
2024-05-26 15:25:18 +0300

    Hw-06 initial commit

--- homework/hw06-proxy-pattern/src/main/java/ru/otus/java/pro/homework06/Program.java
+++ homework/hw06-proxy-pattern/src/main/java/ru/otus/java/pro/homework06/Program.java
@@ -1,8 +1,5 @@
 package ru.otus.java.pro.homework06;
 
-import ru.otus.java.pro.homework05.builder.Product;
-import ru.otus.java.pro.homework05.iterator.Box;
-
 public class Program {
     public static void main(String[] args) {
     }
